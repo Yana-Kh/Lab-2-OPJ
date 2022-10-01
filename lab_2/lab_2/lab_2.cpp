00db45512b2af8162857e5e52b74a56f2cad13ce
Yana-Kh <ynakh2017@mail.ru>
2022-10-01 10:53:35 +0300

    Update lab_2.cpp

--- lab_2/lab_2/lab_2.cpp
+++ lab_2/lab_2/lab_2.cpp
@@ -9,6 +9,11 @@ int main()
     std::cout << "Hello World!\n";
     cout << "5 + 9 = " + 5 + 9;
     int a = 8 - 3;
+    
+    if (a <= 10)
+        cout << "a <= 10";
+    else
+        cout << "ERROR";
 
 
 }
