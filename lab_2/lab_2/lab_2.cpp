7e43dc029cfa43e493e128d80c79a4c1c4c46779
Yana-Kh <ynakh2017@mail.ru>
2022-10-01 10:59:22 +0300

    save

--- lab_2/lab_2/lab_2.cpp
+++ lab_2/lab_2/lab_2.cpp
@@ -6,21 +6,25 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    cout << "5 + 9 = " + 5 + 9;
     int a = 8 - 3;
-    
-    if (a <= 10)
-        cout << "a <= 10";
-    else
-        cout << "ERROR";
-
-    int b =0;
+    int b = 0;
     while (b >= 10)
     {
         cout << "Enter b >= 10: ";
         cin >> b;
     }
+
+
+    if (a <= 10)
+        cout << "a <= 10";
+    else
+        cout << "ERROR";
+
+    std::cout << "Hello World!\n";
+    cout << "5 + 9 = " + 5 + 9;
+
+
+
 
 
 
